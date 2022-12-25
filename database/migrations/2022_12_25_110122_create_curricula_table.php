fd4cbce0bec7445343fd069beb3bc3ffbee95e3f
Olexandr Liakhov <cifroteh2015@gmail.com>
2022-12-25 14:04:24 +0200

    basic migrations and models

--- database/migrations/2022_12_25_110122_create_curricula_table.php
+++ database/migrations/2022_12_25_110122_create_curricula_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('curriculums', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_type')->nullable();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('speciality_id');
+            $table->unsignedBigInteger('subject_id');
+            $table->float('lecture_hours');
+            $table->float('pract_hours');
+            $table->float('lab_hours');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('curriculums');
     }
 };
--- database/migrations/2022_12_25_111050_create_journals_table.php
+++ database/migrations/2022_12_25_111050_create_journals_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('journals', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_type')->nullable();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('professor_id');
+            $table->unsignedBigInteger('subject_id');
+            $table->unsignedBigInteger('speciality_id');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('journals');
     }
 };
