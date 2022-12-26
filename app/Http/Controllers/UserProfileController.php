ced0968a7278bab4972fb1bfc88b4a66a2961135
Olexandr Liakhov <cifroteh2015@gmail.com>
2022-12-26 10:38:22 +0200

    - add users view and controller - models modification

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\UsersExport;
+use App\Models\Professor;
+use App\Models\Specialty;
+use App\Models\Subject;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Facades\Excel;
 
 class UserProfileController extends Controller
 {
@@ -13,7 +20,56 @@ class UserProfileController extends Controller
 
     public function add_users()
     {
-        return view('add_users');
+        $subjects = Subject::get_all();
+        $specialties = Specialty::get_all();
+
+//        dd($subjects, $specialties);
+        return view(
+            'add_users',
+            [
+                'subjects' => $subjects,
+                'specialties' => $specialties,
+            ]
+        );
+    }
+
+    public function save_users(Request $request)
+    {
+        $count = $request->input('val');
+        $temp = [];
+
+        $specialty_id = $request->input('textSelect');
+        if ($specialty_id == 'customOption' || is_null($specialty_id)) {
+            $specialty = new Specialty();
+
+            $specialty->name = $request->input('form-12');
+            $specialty->save();
+
+            $specialty_id = $specialty->id;
+        }
+
+        for ($i = 0; $i < $count; $i++){
+            $password = $request->input('password-'.$i);
+
+            $user = new User();
+            $user->user_type = 2;
+            $user->name = $request->input('name-'.$i);
+            $user->email = $request->input('email-'.$i);
+            $user->password = $request->input('password-'.$i);
+            $user->password =  Hash::make($password);
+            $user->save();
+
+            $user->password = $password;
+            $temp[$i] = $user;
+
+
+            $professor = new Professor();
+            $professor->user_id = $user->id;
+            $professor->speciality_id = $specialty_id;
+            $professor->save();
+        }
+
+        return Excel::download(new UsersExport($temp), 'users.xlsx');
     }
 
 }
--- resources/views/add_users.blade.php
+++ resources/views/add_users.blade.php
@@ -11,22 +11,25 @@
 @endsection
 
 @section('content')
+    <script src="/js/add_users.js"></script>
     <div class="container center mt-5">
         <div class="row g-5">
-            <form action="" method="post" class="needs-validation" novalidate="">
+            <form action="{{ route('save_users') }}" method="post" class="needs-validation" novalidate="">
                 @csrf
                 <div class="row text-center">
                     <div class="col">
-                        <input class="form-control" list="datalistOptions" name="faculty" placeholder="Факультет">
-                        <datalist id="faculty">
-                            <option value="ТЕФ">
-                            <option value="ФІВТ">
-                            <option value="ФБМІ">
-                            <option value="ІПСА">
-                        </datalist>
-                    </div>
-                    <div class="col">
-                        <input type="text" class="form-control" name="group" placeholder="Група" value="" required="">
+                        <div id="textSelectdiv">
+                            <select id="textSelect" name="textSelect" class="select form-control" style="display: inline;" onchange="showInput('textSelect', 'form12', 'inputDiv', 'textSelectdiv')">
+                                <option selected>Спеціальність</option>
+                                @foreach($specialties as $specialty)
+                                    <option value="{{$specialty->id}}">{{$specialty->name}}</option>
+                                @endforeach
+                                <option value="customOption">Інше</option>
+                            </select>
+                        </div>
+                        <div id="inputDiv" class="form-outline disaplayInput">
+                            <input placeholder="Ведіть назву" type="text" id="form12" name="form-12" class="form-control" style="display: none;" onblur="hideInput('textSelect', 'form12', 'inputDiv', 'textSelectdiv')" disabled />
+                        </div>
                     </div>
                     <div class="col">
                         <div class="input-group mb-3">
@@ -34,17 +37,13 @@
                         </div>
                     </div>
                     <div class="col">
-                        <a id="student_count" class="btn btn-success">Підтвердити</a>
+                        <a id="user_count" class="btn btn-success">Підтвердити</a>
                     </div>
                 </div>
 
-                <div class="row reg-form">
+                <div class="row reg-form"></div>
 
-
-                    <script src="/js/students.js"></script>
-                </div>
-
-                <div class="add_students" id="form-btn">
+                <div class="add_students hidden" id="form-btn">
                     <button type="submit" class="btn btn-success std-btn">Підтвердити</button>
                     <button type="button" id="get_pass" class="btn btn-success std-btn">Генерувати паролі</button>
                 </div>
@@ -52,4 +51,89 @@
         </div>
         <div class="" style="min-height: 85px;"></div>
     </div>
+
+    <script>
+        function showInput(select_value_id, input_other_id, div_input_id, select_div_id) {
+            const SelectValue = document.getElementById(select_value_id);
+            const inputOther = document.getElementById(input_other_id);
+            const divInput = document.getElementById(div_input_id);
+            const selectDiv = document.getElementById(select_div_id);
+
+            console.log(SelectValue);
+            console.log(inputOther);
+            console.log(divInput);
+            console.log(selectDiv);
+
+            if (SelectValue.value === 'customOption') {
+                inputOther.style.display='inline';
+                inputOther.removeAttribute('disabled');
+                divInput.classList.remove('disaplayInput');
+                selectDiv.style.display='none';
+                inputOther.focus();
+                SelectValue.disabled = 'true';
+            }
+        }
+
+        function hideInput(select_value_id, input_other_id, div_input_id, select_div_id){
+            const inputOther = document.getElementById(input_other_id);
+            const divInput = document.getElementById(div_input_id);
+            const selectDiv = document.getElementById(select_div_id);
+            const mySelect = $('#' + select_value_id);
+
+            if (inputOther !== null && inputOther.value === "")
+            {
+                console.log(inputOther);
+                inputOther.style.display='none';
+                inputOther.setAttribute('disabled', '');
+                selectDiv.style.display='inline';
+                mySelect.removeAttr('disabled');
+                divInput.classList.add('disaplayInput');
+            }
+        }
+
+        $(document).ready(function () {
+            var count = document.getElementById('val');
+            var btn_submit = document.getElementById('user_count');
+
+            btn_submit.addEventListener('click',(event) => {
+                form_gen(count.value);
+            });
+
+            function form_gen(count){
+                const form = document.querySelector(".reg-form");
+
+                let users_form = "";
+
+                if (count > 0) {
+                    for (let x = 0; x < count; x++) {
+                        users_form += `
+                            <div class="row">
+                                <div class="col">
+                                    <div class="input-group mb-3">
+                                        <input type="text" name="name-${x}" id="name-${x}" class="form-control" placeholder="ПІБ" aria-label="Username">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="input-group mb-3">
+                                        <input type="text" name="email-${x}" id="email-${x}" class="form-control" placeholder="email">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="input-group mb-3">
+                                        <input type="password" name="password-${x}" id="password-${x}" class="form-control" placeholder="password" aria-label="Username">
+                                    </div>
+                                </div>
+                            </div>
+                        `;
+                    }
+
+                    form.innerHTML = users_form;
+                    $('#form-btn').removeClass('hidden')
+                } else {
+                    form.innerHTML = users_form;
+                    $('#form-btn').addClass('hidden')
+                }
+            }
+        });
+    </script>
 @endsection
